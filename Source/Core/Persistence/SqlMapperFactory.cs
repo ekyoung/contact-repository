ca95d95c6b8d50b347b0cf76d02b6807709f8919
Ethan Young <ethan.young@tenovernine.com>
2013-12-07 15:34:43 -0700

    Un-hardcode the sqlconnection to use for the tests.

--- Source/Core/Persistence/SqlMapperFactory.cs
+++ Source/Core/Persistence/SqlMapperFactory.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections.Specialized;
+using System.Configuration;
 using System.Xml;
+using EthanYoung.ContactRepository.Properties;
 using IBatisNet.Common.Utilities;
 using IBatisNet.DataMapper.Configuration;
 
@@ -7,25 +10,24 @@ namespace EthanYoung.ContactRepository.Persistence
 {
     public class SqlMapperFactory
     {
-        private static volatile IBatisNet.DataMapper.ISqlMapper _mapper;
-
-        static SqlMapperFactory()
+        public static IBatisNet.DataMapper.ISqlMapper GetMapper()
         {
-            var properties = new NameValueCollection { { "ConnectionString", @"Data Source=localhost;Initial Catalog=ContactRepositoryTests;Integrated Security=True" } };
+            string connectionStringName = string.Format("LocalDev-{0}-{1}", Environment.MachineName, Environment.UserName);
+            ConnectionStringSettings connectionStringSettings = ConfigurationManager.ConnectionStrings[connectionStringName] ??
+                                                                ConfigurationManager.ConnectionStrings[Settings.Default.DefaultConnectionStringName];
+
+            var properties = new NameValueCollection { { "ConnectionString", connectionStringSettings.ConnectionString } };
 
             var builder = new DomSqlMapBuilder { Properties = properties };
 
             XmlDocument sqlMapConfig = Resources.GetEmbeddedResourceAsXmlDocument("EthanYoung.ContactRepository.Persistence.sqlMap.config, EthanYoung.ContactRepository");
-            _mapper = builder.Configure(sqlMapConfig);
+
             //If I can get the type handler registered before the result maps are built, I'm still stuck with having
             //to register this thing for each type, but I don't need a different handler for each type
             //_mapper.TypeHandlerFactory.Register(typeof(MemberStatus), new RegisteredValueObjectTypeHandler());
             //((ResultMap)_mapper.ResultMaps["Member.MemberRM"]).Properties[12].TypeHandler = new RegisteredValueObjectTypeHandler();
-        }
-
-        public static IBatisNet.DataMapper.ISqlMapper GetMapper()
-        {
-            return _mapper;
+            
+            return builder.Configure(sqlMapConfig);
         }
     }
 }
