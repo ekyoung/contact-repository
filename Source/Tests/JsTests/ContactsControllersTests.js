3a8ad4ed033829335e3ceea118c6b0b956c51c3d
Ethan Young <ethan.young@tenovernine.com>
2013-12-11 20:48:55 -0700

    Setup tests for the javascript

---
+++ Source/Tests/JsTests/ContactsControllersTests.js
@@ -0,0 +1,39 @@
+﻿describe('Contacts Controllers', function () {
+    beforeEach(module('contactsApp'));
+
+    describe('contactsController', function() {
+        //Nothing here yet
+    });
+
+    describe('listController', function () {
+        var $httpBackend, $scope, contactsData, createController;
+
+        var apiRoot = '/api';
+        
+        beforeEach(inject(function ($injector) {
+            // Set up the mock http service responses
+            $httpBackend = $injector.get('$httpBackend');
+
+            $scope = $injector.get('$rootScope');
+
+            contactsData = {};
+            
+            // The $controller service is used to create instances of controllers
+            var $controller = $injector.get('$controller');
+
+            createController = function() {
+                return $controller('listController', { $scope: $scope, contactsData: contactsData });
+            };
+        }));
+
+        it('should set an array of contacts on the scope when given an array of contacts', function () {
+            var contacts = [{ FirstName: 'Joe', LastName: 'One' }, { FirstName: 'Frank', LastName: 'Two' }];
+            $httpBackend.when('GET', apiRoot + '/contacts').respond(contacts);
+            var controller = createController();
+            $httpBackend.flush();
+            
+            expect($scope.contacts.length).toBe(2);
+        });
+
+    });
+});
