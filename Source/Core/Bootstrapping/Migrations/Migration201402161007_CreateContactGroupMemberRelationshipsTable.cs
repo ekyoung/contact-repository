f5dc6b8684d28cc2a6067342ab616f18baf7649b
Ethan Young <ethan.young@tenovernine.com>
2014-02-16 10:19:36 -0700

    Add a table to hold Contact Group Member Relationships.

---
+++ Source/Core/Bootstrapping/Migrations/Migration201402161007_CreateContactGroupMemberRelationshipsTable.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+
+namespace EthanYoung.ContactRepository.Bootstrapping.Migrations
+{
+    [Migration(201402161007)]
+    public class Migration201402161007_CreateContactGroupMemberRelationshipsTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("ContactGroupMemberRelationships")
+                .WithColumn("ContactGroupMemberId").AsInt64()
+                .WithColumn("Relationship").AsString().NotNullable();
+
+            Create.ForeignKey("FK_ContactGroupMemberRelationships_ContactGroupMembers")
+                .FromTable("ContactGroupMemberRelationships").ForeignColumn("ContactGroupMemberId")
+                .ToTable("ContactGroupMembers").PrimaryColumn("ContactGroupMemberId");
+        }
+
+        public override void Down()
+        {
+            Delete.Table("ContactGroupMemberRelationships");
+        }
+    }
+}
